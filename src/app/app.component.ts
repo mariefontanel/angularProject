3bd499dccbf80f6211ee6c62e9c2a5bf85d2e81a
mariefontanel <marie.fontanel.pro@gmail.com>
2018-10-31 10:33:50 +0100

    ajout d'un compteur de secondes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, interval} from 'rxjs';
 
 @Component({
   selector: 'app-root', // nom balise html perso, doit être unique
   templateUrl: './app.component.html', // url du template
   styleUrls: ['./app.component.css'] // array des url des feuilles de style
 })
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit {
 
+  secondes: number;
+
+  constructor() {}
+  ngOnInit() {
+    const counter = interval(1000);
+    counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Une erreur a été rencontrée !' + error);
+      },
+      () => {
+        console.log('Observable complétée !');
+      }
+    );
   }
 }
